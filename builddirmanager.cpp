h4tr3d/cmakeprojectmanager2

commit f710eb2994170411de20431660262305e0d46e64

Fix building after merge

builddirmanager.cpp
@@ -465,8 +465,7 @@ void BuildDirManager::startCMake(CMakeTool *tool, const QString &generator,
     Utils::QtcProcess::addArg(&args, srcDir);
     if (!generator.isEmpty())
         Utils::QtcProcess::addArg(&args, QString::fromLatin1("-G%1").arg(generator));
-    Utils::QtcProcess::addArgs(&args, toArguments(config, kit()));
-    Utils::QtcProcess::addArgs(&args, toolchain.arguments(toArguments(config), workDirectory().toString()));
+    Utils::QtcProcess::addArgs(&args, toolchain.arguments(toArguments(config, kit()), workDirectory().toString()));
 
     ProjectExplorer::TaskHub::clearTasks(ProjectExplorer::Constants::TASK_CATEGORY_BUILDSYSTEM);
 

commit 25ff6048d28a62e6aa0708adf110212590a6771f

Fix cmake arguments passing
Was broken by f710eb2994170411de20431660262305e0d46e64

builddirmanager.cpp
@@ -465,6 +465,7 @@ void BuildDirManager::startCMake(CMakeTool *tool, const QString &generator,
     Utils::QtcProcess::addArg(&args, srcDir);
     if (!generator.isEmpty())
         Utils::QtcProcess::addArg(&args, QString::fromLatin1("-G%1").arg(generator));
+    Utils::QtcProcess::addArgs(&args, toArguments(config, kit()));
     Utils::QtcProcess::addArgs(&args, toolchain.arguments(toArguments(config, kit()), workDirectory().toString()));
 
     ProjectExplorer::TaskHub::clearTasks(ProjectExplorer::Constants::TASK_CATEGORY_BUILDSYSTEM);