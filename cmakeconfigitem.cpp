dc3fb49c121a05202cf51a1d6656aa6a20b03ca7
Alexander S. Drozdov <adrozdoff@gmail.com>
2016-06-07 15:51:08 +1000

    Revert "Revert "CMake: Handle STATIC types in CMakeCache.txt""
    This reverts commit 94122f7d68eb4589eaad68ed6149a775933c3b34.

--- cmakeconfigitem.cpp
+++ cmakeconfigitem.cpp
@@ -138,6 +138,8 @@ CMakeConfigItem CMakeConfigItem::fromString(const QString &s)
             t = CMakeConfigItem::BOOL;
         else if (type == QLatin1String("INTERNAL"))
             t = CMakeConfigItem::INTERNAL;
+        else if (type == QLatin1String("STATIC"))
+            t = CMakeConfigItem::STATIC;
 
         item.key = key.toUtf8();
         item.type = t;
@@ -148,7 +150,7 @@ CMakeConfigItem CMakeConfigItem::fromString(const QString &s)
 
 QString CMakeConfigItem::toString() const
 {
-    if (key.isEmpty())
+    if (key.isEmpty() || type == CMakeProjectManager::CMakeConfigItem::STATIC)
         return QString();
 
     QString typeStr;
