qt-creator/qt-creator

CMakeBuildStep: Document that the buildconfiguration will be non-null
Change-Id: I27be487f2a662df8cf41152d6c7ffd0f40151061
Reviewed-by: hjk <hjk@qt.io>

src/plugins/cmakeprojectmanager/cmakebuildstep.cpp
@@ -155,7 +155,8 @@ bool CMakeBuildStep::init()
 {
     bool canInit = true;
     CMakeBuildConfiguration *bc = cmakeBuildConfiguration();
-    if (bc && !bc->isEnabled()) {
+    QTC_ASSERT(bc, return false);
+    if (!bc->isEnabled()) {
         emit addTask(BuildSystemTask(Task::Error,
                           tr("CMakeProjectManager::CMakeBuildStep")));
         canInit = false;

h4tr3d/cmakeprojectmanager2

cmakebuildstep.cpp
@@ -155,7 +155,8 @@ bool CMakeBuildStep::init()
 {
     bool canInit = true;
     CMakeBuildConfiguration *bc = cmakeBuildConfiguration();
-    if (bc && !bc->isEnabled()) {
+    TC_ASSERT(bc, return false);
+    if (!bc->isEnabled()) {
         emit addTask(
             BuildSystemTask(Task::Error, tr("The build configuration is currently disabled.")));
         canInit = false;