ca1231c2981de64921cd96dd0af56b89496fa86d
David Schulz <david.schulz@qt.io>
2020-04-17 08:53:16 +0200

    CMake: honor use jom instead of nmake setting
    Only search for the jom cmake generator if this setting is
    enabled.
    
    Change-Id: Id8eed91d47f1f6ca2375fe0d169b21168c5ca4ff
    Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

--- cmakekitinformation.cpp
+++ cmakekitinformation.cpp
@@ -33,8 +33,11 @@
 
 #include <coreplugin/icore.h>
 #include <coreplugin/variablechooser.h>
-#include <projectexplorer/toolchain.h>
 #include <projectexplorer/kitinformation.h>
+#include <projectexplorer/projectexplorer.h>
+#include <projectexplorer/projectexplorersettings.h>
+#include <projectexplorer/task.h>
+#include <projectexplorer/toolchain.h>
 #include <qtsupport/baseqtversion.h>
 #include <qtsupport/qtkitinformation.h>
 #include <projectexplorer/projectexplorerconstants.h>
@@ -657,6 +660,21 @@ QVariant CMakeGeneratorKitAspect::defaultValue(const Kit *k) const
                                   return g.matches("NMake Makefiles")
                                          || g.matches("NMake Makefiles JOM");
                               });
+            if (ProjectExplorerPlugin::projectExplorerSettings().useJom) {
+                it = std::find_if(known.constBegin(),
+                                  known.constEnd(),
+                                  [extraGenerator](const CMakeTool::Generator &g) {
+                                      return g.matches("NMake Makefiles JOM", extraGenerator);
+                                  });
+            }
+
+            if (it == known.constEnd()) {
+                it = std::find_if(known.constBegin(),
+                                  known.constEnd(),
+                                  [extraGenerator](const CMakeTool::Generator &g) {
+                                      return g.matches("NMake Makefiles", extraGenerator);
+                                  });
+            }
         }
     } else {
         // Unix-oid OSes:
