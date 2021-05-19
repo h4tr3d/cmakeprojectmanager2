cdca5c23c97a5f6754e41b8e52c65f352443831b
Cristian Adam <cristian.adam@qt.io>
2021-05-19 17:22:29 +0200

    CMakePM: Do not filter out all UTILITY targets
    add_custom_target will be set as UTILITY target, which one might
    want to reference as "cm <target>" or "--target <target>".
    
    Filter out instead the "_autogen" and "_autogen_timestamp_deps".
    
    Fixes: QTCREATORBUG-25726
    Change-Id: Ia73d6e87e4b230114d068649964792ff3f626378
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- cmakebuildsystem.cpp
+++ cmakebuildsystem.cpp
@@ -1033,7 +1033,7 @@ const QList<BuildTargetInfo> CMakeBuildSystem::appTargets() const
     const bool forAndroid = DeviceTypeKitAspect::deviceTypeId(kit())
                             == Android::Constants::ANDROID_DEVICE_TYPE;
     for (const CMakeBuildTarget &ct : m_buildTargets) {
-        if (ct.targetType == UtilityType)
+        if (CMakeBuildSystem::filteredOutTarget(ct))
             continue;
 
         if (ct.targetType == ExecutableType || (forAndroid && ct.targetType == DynamicLibraryType)) {
@@ -1064,11 +1064,10 @@ const QList<BuildTargetInfo> CMakeBuildSystem::appTargets() const
 
 QStringList CMakeBuildSystem::buildTargetTitles() const
 {
-    auto nonUtilityTargets = filtered(m_buildTargets, [this](const CMakeBuildTarget &target){
-        return target.targetType != UtilityType ||
-               CMakeBuildStep::specialTargets(usesAllCapsTargets()).contains(target.title);
+    auto nonAutogenTargets = filtered(m_buildTargets, [this](const CMakeBuildTarget &target){
+        return !CMakeBuildSystem::filteredOutTarget(target);
     });
-    return transform(nonUtilityTargets, &CMakeBuildTarget::title);
+    return transform(nonAutogenTargets, &CMakeBuildTarget::title);
 }
 
 const QList<CMakeBuildTarget> &CMakeBuildSystem::buildTargets() const
@@ -1088,6 +1087,12 @@ CMakeConfig CMakeBuildSystem::parseCMakeCacheDotTxt(const Utils::FilePath &cache
     if (!errorMessage->isEmpty())
         return {};
     return result;
+}
+
+bool CMakeBuildSystem::filteredOutTarget(const CMakeBuildTarget &target)
+{
+    return target.title.endsWith("_autogen") ||
+           target.title.endsWith("_autogen_timestamp_deps");
 }
 
 bool CMakeBuildSystem::isMultiConfig() const
