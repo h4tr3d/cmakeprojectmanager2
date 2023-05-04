cristianadam/qt-creator

Avoid creation of temporary lists of ProjectNodes
Change-Id: Iac84f7f95291fb3b12a071a066d358aaab309c52

src/plugins/cmakeprojectmanager/fileapidataextractor.cpp
@@ -497,11 +497,8 @@ FolderNode *createSourceGroupNode(const QString &sourceGroupName,
         const QStringList parts = sourceGroupName.split("\\");
 
         for (const QString &p : parts) {
-            FolderNode *existingNode = Utils::findOrDefault(currentNode->folderNodes(),
-                                                            [&p](const FolderNode *fn) {
-                                                                return fn->displayName() == p;
-                                                            });
-
+            FolderNode *existingNode = currentNode->findFolderNode(
+                [&p](const FolderNode *fn) { return fn->displayName() == p; });
             if (!existingNode) {
                 auto node = createCMakeVFolder(sourceDirectory, Node::DefaultFolderPriority + 5, p);
                 node->setListInProject(false);

h4tr3d/cmakeprojectmanager2

Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

fileapidataextractor.cpp
@@ -497,11 +497,8 @@ FolderNode *createSourceGroupNode(const QString &sourceGroupName,
         const QStringList parts = sourceGroupName.split("\\");
 
         for (const QString &p : parts) {
-            FolderNode *existingNode = Utils::findOrDefault(currentNode->folderNodes(),
-                                                            [&p](const FolderNode *fn) {
-                                                                return fn->displayName() == p;
-                                                            });
-
+            FolderNode *existingNode = currentNode->findChildFolderNode(
+                [&p](const FolderNode *fn) { return fn->displayName() == p; });
             if (!existingNode) {
                 auto node = createCMakeVFolder(sourceDirectory, Node::DefaultFolderPriority + 5, p);
                 node->setListInProject(false);